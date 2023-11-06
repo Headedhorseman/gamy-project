1cc477fc5a82200e7d8ea83c81fc23464e2cd7fe
Headedhorseman <149863215+Headedhorseman@users.noreply.github.com>
2023-11-06 22:33:07 +0700

    Add files via upload
    third commit, 11-6-2023. decided on game. main clicking mechanic done. ready for the easy part... the maybe easy part

--- simplegame.py
+++ simplegame.py
@@ -1,57 +1,65 @@
 import pygame
 import time
 import random
+from pygame.locals import *
 pygame.font.init()
-
-WIDTH, HEIGHT = 1200, 700
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Smash Down")
-
-Testbg = pygame.image.load("test.jpeg")
-# menu bg
-# main game bg
-
 font = pygame.font.SysFont("arial", 18)
-
-# monster1 = ???
-
-
-
-
-
-
-def drawstart():
-    WIN.blit(Testbg, (0, 0))
-
-    controls_text1 = font.render("Left mouse button     : SMASH!!! (destroy the monsters that run by)", True, (255, 255, 255))
-    controls_text2 = font.render("Right mouse button   : parry (redirect incoming dangers)", True, (255, 255, 255))
-    controls_text3 = font.render("Middle mouse button : rescue (save the innocent)", True, (255, 255, 255))
-
-    WIN.blit(controls_text1, (10,10))
-    WIN.blit(controls_text2, (10,30))
-    WIN.blit(controls_text3, (10,50))
+WIDTH, HEIGHT = 500, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Cheap SCookie Clicker")
+BG = pygame.transform.scale(pygame.image.load("CHBG.jpg"), (WIDTH, HEIGHT))
+cookie_img = pygame.transform.scale(pygame.image.load("cookie.png"), (100, 100))
+mula = 0
+power = 1
 
 
-    pygame.display.update() 
+class button():
+    def __init__(self, x, y, image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
+        self.clicked = False
 
+    def drawbutton(self):
+        act = False
+        mouse_pos = pygame.mouse.get_pos()
 
+        if self.rect.collidepoint(mouse_pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                act = True
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+        
+        WIN.blit(self.image, (self.rect.x, self.rect.y))
+        return act
+    
+clicking_cookie = button(20, 200, cookie_img)
+
+def drawinitial():
+    WIN.blit(BG, (0, 0))
+    muns = font.render(f"{mula} Cookies", 1, "white")
+    WIN.blit(muns, (10, 10))
 
 def main():
+    global mula
     run = True
-    drawstart()
     clock = pygame.time.Clock()
+
     while run :
+        drawinitial()
         clock.tick(60)
+
+        if clicking_cookie.drawbutton():
+            mula += power
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
 
-        keys = pygame.key.get_pressed()
-    # if right click atk
-    #    left        block/parry
-    #    and midd    rescue
+        pygame.display.update()
 
     pygame.quit()
 
